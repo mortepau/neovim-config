89d36d111aa87d767ba31be7fd40e8c6d6bd5943
Morten Paulsen <morten.p96@gmail.com>
2021-03-26 18:18:07 +0100

    Add more syntax/highlight functions

--- lua/mortepau/globals/syntax.lua
+++ lua/mortepau/globals/syntax.lua
@@ -11,6 +11,65 @@ function mortepau.syntax.stack()
   return vim.tbl_map(function(v) return vim.fn.synIDattr(v, 'name') end, synstack)
 end
 
+function mortepau.syntax.get_highlight(group)
+  if vim.fn.hlexists(group) == 0 then return {} end
+
+  local highlights = { color = {}, attribute = {}}
+  local colors = {
+    'fg',
+    'bg',
+    'sp',
+  }
+  local attributes = {
+    'bold',
+    'italic',
+    'inverse',
+    'standout',
+    'underline',
+    'strikethrough'
+  }
+
+  local synIDattr = vim.fn.synIDattr
+  local group_id = vim.fn.synIDtrans(vim.fn.hlID(group))
+
+  for _, color in ipairs(colors) do
+    local gui_color = synIDattr(group_id, color, 'gui')
+    local cterm_color = synIDattr(group_id, color, 'cterm')
+    highlights.color['gui' .. color] = gui_color ~= '' and gui_color or nil
+    highlights.color['cterm' .. color] = cterm_color ~= '' and cterm_color or nil
+  end
+
+  for _, attribute in ipairs(attributes) do
+    highlights.attribute[attribute] = synIDattr(group_id, attribute) == 1 and true or nil
+  end
+
+  return highlights
+end
+
+function mortepau.syntax.format_highlight(name, highlight)
+  local prefix = vim.o.termguicolors == 1 and 'gui' or 'cterm'
+  local s = ''
+
+  -- Move the highlight definition from name, if no name is provided
+  if not highlight then
+    highlight = name
+  else
+    s = name .. ' '
+  end
+
+  if #highlight.attribute > 0 then
+    s = s .. prefix .. '=' .. table.concat(highlight.attribute, ',') .. ' '
+  end
+
+  for name, color in pairs(highlight.color) do
+    s = s .. name .. '=' .. color .. ' '
+  end
+
+  s = s:sub(1, -2)
+
+  return s
+end
+
 function mortepau.syntax.highlight_whitespace()
   local group_name = 'HighlightWhitespace'
   if vim.fn.exists('#' .. group_name) then
