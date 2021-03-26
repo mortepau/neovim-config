2faa7c41345f990d7482d92e47ac25da6c86ca13
Morten Paulsen <morten.p96@gmail.com>
2021-03-26 20:12:42 +0100

    Add a once autocmd to refresh nvim-tree on first load

--- lua/mortepau/plugins/nvim_tree.lua
+++ lua/mortepau/plugins/nvim_tree.lua
@@ -2,4 +2,7 @@ vim.nnoremap('<leader>ft', '<cmd>NvimTreeToggle<Cr>')
 
 vim.g.nvim_tree_quit_on_open = true
 
-require('nvim-tree').refresh()
+vim.augroup('NvimTree', {
+  { 'Filetype', 'NvimTree', 'lua require("nvim-tree").refresh()', { once = true } }
+})
+-- require('nvim-tree').refresh()
