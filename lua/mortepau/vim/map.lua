d237194d4a4333cb723564ca86767132ac99b521
Morten Paulsen <morten.p96@gmail.com>
2021-03-30 15:49:37 +0200

    Fix so that map doesn't modify the opts table

--- lua/mortepau/vim/map.lua
+++ lua/mortepau/vim/map.lua
@@ -27,68 +27,68 @@ end
 -- Recursive mappings
 
 function M.map(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = nil
   _map(MODE.NORMAL_VISUAL_OPERATOR, lhs, rhs, opts)
 end
 
 function M.nmap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = nil
   _map(MODE.NORMAL, lhs, rhs, opts)
 end
 
 function M.vmap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = nil
   _map(MODE.VISUAL_SELECT, lhs, rhs, opts)
 end
 
 function M.xmap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = nil
   _map(MODE.VISUAL, lhs, rhs, opts)
 end
 
 function M.smap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = nil
   _map(MODE.SELECT, lhs, rhs, opts)
 end
 
 function M.omap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = nil
   _map(MODE.OPERATOR, lhs, rhs, opts)
 end
 
 -- Using 'ic' instead of '!' as a workaround
 function M.icmap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = nil
   _map(MODE.INSERT_COMMAND, lhs, rhs, opts)
 end
 
 function M.imap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = nil
   _map(MODE.INSERT, lhs, rhs, opts)
 end
 
 function M.cmap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = nil
   _map(MODE.COMMAND, lhs, rhs, opts)
 end
 
 function M.lmap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = nil
   _map(MODE.INSERT_COMMAND_LANG, lhs, rhs, opts)
 end
 
 function M.tmap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = nil
   _map(MODE.TERMINAL, lhs, rhs, opts)
 end
@@ -96,68 +96,68 @@ end
 -- Non-recursive mappings
 
 function M.noremap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = true
   _map(MODE.NORMAL_VISUAL_OPERATOR, lhs, rhs, opts)
 end
 
 function M.nnoremap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = true
   _map(MODE.NORMAL, lhs, rhs, opts)
 end
 
 function M.vnoremap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = true
   _map(MODE.VISUAL_SELECT, lhs, rhs, opts)
 end
 
 function M.xnoremap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = true
   _map(MODE.VISUAL, lhs, rhs, opts)
 end
 
 function M.snoremap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = true
   _map(MODE.SELECT, lhs, rhs, opts)
 end
 
 function M.onoremap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = true
   _map(MODE.OPERATOR, lhs, rhs, opts)
 end
 
 -- Using 'ic' instead of '!' as a workaround
 function M.icnoremap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = true
   _map(MODE.INSERT_COMMAND, lhs, rhs, opts)
 end
 
 function M.inoremap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = true
   _map(MODE.INSERT, lhs, rhs, opts)
 end
 
 function M.cnoremap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = true
   _map(MODE.COMMAND, lhs, rhs, opts)
 end
 
 function M.lnoremap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = true
   _map(MODE.INSERT_COMMAND_LANG, lhs, rhs, opts)
 end
 
 function M.tnoremap(lhs, rhs, opts)
-  opts = opts or {}
+  opts = vim.deepcopy(opts) or {}
   opts.noremap = true
   _map(MODE.TERMINAL, lhs, rhs, opts)
 end
